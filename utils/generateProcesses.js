7cef3e2c228ec9d08ac82bb4d85a5541452e3239
Zane Liu <zaneliu316@gmail.com>
2025-03-06 00:01:39 -0600

    create folder and file for random number generation

---
+++ utils/generateProcesses.js
@@ -0,0 +1,7 @@
+export function generateProcesses(count){
+    return Array.from({ length: count }, (_, i) => ({
+        id: i +1,
+        arrivalTime: Math.floor(Math.random() * 10),
+        burstTime: Math.floor(Math.random() * 10) + 1,
+    }))
+}

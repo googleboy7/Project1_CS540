026a6d2fc62350995b1c6caae96a3e076a14a063
Zane Liu <zaneliu316@gmail.com>
2025-03-13 18:28:10 -0500

    changes to fifo.js file to get the turnaround time and the waiting time

--- cpu-scheduler/src/algorithms/fifo.js
+++ cpu-scheduler/src/algorithms/fifo.js
@@ -1,11 +1,29 @@
-export function fifo(processes){
+export function fifo(processes) {
     let time = 0;
     let schedule = [];
+    
+    // Sort processes by arrival time
     processes.sort((a, b) => a.arrivalTime - b.arrivalTime);
+    
     processes.forEach(process => {
+        // If the current time is less than the process's arrival time, we update the time to the arrival time
         if (time < process.arrivalTime) time = process.arrivalTime;
-        schedule.push({ id: process.id, startTime: time, endTime: time = process.burstTime});
-        time += process.burstTime;
+        
+        // Add the process to the schedule with correct start and end time
+        let startTime = time;
+        let endTime = time + process.burstTime;
+        
+        // Update the time for the next process
+        time = endTime;
+
+        schedule.push({
+            id: process.id,
+            arrivalTime: process.arrivalTime,
+            burstTime: process.burstTime,
+            startTime: startTime,
+            finishTime: endTime,  // Fix to use the correct finish time
+        });
     });
+
     return schedule;
 }
